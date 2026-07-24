20ac57f363ac1e3dee4d0b474b46f2812277a0ab
Yunus Emre Can Sümeralp <emresmrlp@gmail.com>
2026-07-24 22:18:04 +0300

    fix(auth): database envs and check_same_thread error fixed

--- services/auth/srcs/database.py
+++ services/auth/srcs/database.py
@@ -2,9 +2,9 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-POSTGRES_AUTH_DB = os.environ.get("POSTGRES_AUTH_DB", "auth")
-POSTGRES_USER = os.environ.get("POSTGRES_USER", "auth")
-POSTGRES_PASSWORD_FILE = os.environ.get("POSTGRES_PASSWORD_FILE")
+POSTGRES_AUTH_DB = os.environ.get("DB_NAME", "auth")
+POSTGRES_USER = os.environ.get("DB_USER", "auth")
+POSTGRES_PASSWORD_FILE = os.environ.get("DB_PASSWORD_FILE")
 
 with open(POSTGRES_PASSWORD_FILE) as f:
     POSTGRES_PASSWORD = f.read().strip()
@@ -14,8 +14,7 @@ DATABASE_URL = (
     f"@postgres-auth/{POSTGRES_AUTH_DB}"
 )
 
-connect_args = {"check_same_thread": False}
-engine = create_engine(DATABASE_URL, connect_args=connect_args)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 class Base(DeclarativeBase):
