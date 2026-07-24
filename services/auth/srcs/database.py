2d5223d15728731920364d9515ee8d0f69378508
Ismail <cilginsinek58@gmail.com>
2026-07-24 18:29:08 +0300

    refactor(auth): refactor database url schema

--- services/auth/srcs/database.py
+++ services/auth/srcs/database.py
@@ -2,7 +2,17 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg://postgres:123456@localhost/auth_service")
+POSTGRES_AUTH_DB = os.environ.get("POSTGRES_AUTH_DB", "auth")
+POSTGRES_USER = os.environ.get("POSTGRES_USER", "auth")
+POSTGRES_PASSWORD_FILE = os.environ.get("POSTGRES_PASSWORD_FILE")
+
+with open(POSTGRES_PASSWORD_FILE) as f:
+    POSTGRES_PASSWORD = f.read().strip()
+
+DATABASE_URL = (
+    f"postgresql+psycopg://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+    f"@postgres-auth/{POSTGRES_AUTH_DB}"
+)
 
 connect_args = {"check_same_thread": False}
 engine = create_engine(DATABASE_URL, connect_args=connect_args)
