c4828b711270f86d05fdfbff8fcb7cfab175206b
yemreaycicek <yunus.e.aycicek@gmail.com>
2026-08-15 12:09:03 +0300

    refactor(routes): update HomePage import after folder move

--- services/frontend/srcs/src/routes/AppRoutes.tsx
+++ services/frontend/srcs/src/routes/AppRoutes.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router'
 
 import { paths } from './paths'
 import { AppLayout } from '@/components/layout/AppLayout'
-import HomePage from '@/pages/HomePage'
+import HomePage from '@/pages/HomePage/HomePage'
 import NotFoundPage from '@/pages/NotFoundPage'
 
 export function AppRoutes() {
