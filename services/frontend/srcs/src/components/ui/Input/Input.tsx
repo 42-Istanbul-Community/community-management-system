424180caf7433720a2f22061ba204bf51d317b31
yemreaycicek <yunus.e.aycicek@gmail.com>
2026-08-18 18:59:07 +0300

    chore(ui): self-close the input element

--- services/frontend/srcs/src/components/ui/Input/Input.tsx
+++ services/frontend/srcs/src/components/ui/Input/Input.tsx
@@ -2,5 +2,5 @@ import { inputStyles } from './Input.styles'
 import type { InputProps } from './Input.types'
 
 export function Input({ hasError, className, ...props }: InputProps) {
-  return <input className={inputStyles(hasError, className)} {...props}></input>
+  return <input className={inputStyles(hasError, className)} {...props} />
 }
