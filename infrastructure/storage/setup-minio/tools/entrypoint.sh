097421323359bcd02bb38faf13c81bbf2c9a9988
Yunus Emre Can Sümeralp <emresmrlp@gmail.com>
2026-08-18 16:25:39 +0300

    feat(setup-minio): add more buckets and service secrets

--- infrastructure/storage/setup-minio/tools/entrypoint.sh
+++ infrastructure/storage/setup-minio/tools/entrypoint.sh
@@ -3,6 +3,12 @@
 set -e
 
 MINIO_ROOT_PASSWORD=$(cat "$MINIO_ROOT_PASSWORD_FILE")
+CONTENT_ACCESS_KEY=$(cat "$MINIO_CONTENT_ACCESS_KEY_FILE")
+CONTENT_SECRET_KEY=$(cat "$MINIO_CONTENT_SECRET_KEY_FILE")
+ID_ACCESS_KEY=$(cat "$MINIO_ID_ACCESS_KEY_FILE")
+ID_SECRET_KEY=$(cat "$MINIO_ID_SECRET_KEY_FILE")
+COMMUNITY_ACCESS_KEY=$(cat "$MINIO_COMMUNITY_ACCESS_KEY_FILE")
+COMMUNITY_SECRET_KEY=$(cat "$MINIO_COMMUNITY_SECRET_KEY_FILE")
 
 until mc alias set minio \
     "http://minio:9000" \
@@ -12,4 +18,14 @@ do
     sleep 2
 done
 
+mc mb --ignore-existing minio/id
+mc mb --ignore-existing minio/community
 mc mb --ignore-existing minio/content
+
+mc admin user add minio content-service "$CONTENT_ACCESS_KEY" "$CONTENT_SECRET_KEY"
+mc admin user add minio id-service "$ID_ACCESS_KEY" "$ID_SECRET_KEY"
+mc admin user add minio community-service "$COMMUNITY_ACCESS_KEY" "$COMMUNITY_SECRET_KEY"
+
+mc admin policy attach minio readwrite --user content-service
+mc admin policy attach minio readwrite --user id-service
+mc admin policy attach minio readwrite --user community-service
