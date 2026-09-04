024536a8d40aeb459954a2ed8e2bf97db88e1bfc
yemreaycicek <yunus.e.aycicek@gmail.com>
2026-09-04 20:21:18 +0300

    feat(communities): enhance useCreateCommunity to invalidate communityRequests on success

--- services/frontend/srcs/src/features/communities/hooks/useCreateCommunity.ts
+++ services/frontend/srcs/src/features/communities/hooks/useCreateCommunity.ts
@@ -1,6 +1,21 @@
 import { createCommunity } from '@/features/communities/api'
-import { useMutation } from '@tanstack/react-query'
+import { useAuthStore } from '@/stores'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+// export function useCreateCommunity() {
+//   return useMutation({ mutationFn: createCommunity })
+// }
 
 export function useCreateCommunity() {
-  return useMutation({ mutationFn: createCommunity })
+  const queryClient = useQueryClient()
+  const userId = useAuthStore((state) => state.user?.id)
+
+  return useMutation({
+    mutationFn: createCommunity,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['communityRequests', userId],
+      })
+    },
+  })
 }
