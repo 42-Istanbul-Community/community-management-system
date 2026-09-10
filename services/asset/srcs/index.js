996acf5eba39f9436aeec1fe86b7be23042fcb10
Ismail <cilginsinek58@gmail.com>
2026-09-11 00:01:39 +0300

    feat(BE): enable credentials in CORS options for multiple services

--- services/asset/srcs/index.js
+++ services/asset/srcs/index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 const CORS_OPTIONS = {
   origin: process.env.DOMAIN_NAME ? new RegExp(`^https?:\\/\\/(.*\\.)?${process.env.DOMAIN_NAME.replace(/\./g, '\\.')}$`) : "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
 };
 
 const app = express();
--- services/id/srcs/index.js
+++ services/id/srcs/index.js
@@ -12,6 +12,7 @@ const CORS_OPTIONS = {
     )
     : "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
 };
 
 const app = express();
