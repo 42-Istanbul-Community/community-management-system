6093d76885d09ecb3f2f8f3e6881f6f51a4a510d
alikosaca <alikosaca0@gmail.com>
2026-09-11 01:41:12 +0300

    fix(cors): restrict content CORS to DOMAIN_NAME instead of wildcard

--- services/content/srcs/index.js
+++ services/content/srcs/index.js
@@ -9,7 +9,11 @@ const setUser = require('./utils/setUser');
 const PORT = process.env.PORT || 8000;
 
 const CORS_OPTIONS = {
-  origin: '*',
+  origin: process.env.DOMAIN_NAME
+    ? new RegExp(
+        `^https?:\\/\\/(.*\\.)?${process.env.DOMAIN_NAME.replace(/\./g, "\\.")}$`,
+      )
+    : "*",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-User-ID', 'X-User-Role'],
 };
