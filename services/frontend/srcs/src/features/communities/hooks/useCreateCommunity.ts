08ec61753ebd0ee5b35501ed322bf7b6f5a3339a
yemreaycicek <yunus.e.aycicek@gmail.com>
2026-09-13 10:14:36 +0300

    chore(community): remove commented-out code

--- services/frontend/srcs/src/features/communities/hooks/useCreateCommunity.ts
+++ services/frontend/srcs/src/features/communities/hooks/useCreateCommunity.ts
@@ -2,10 +2,6 @@ import { createCommunity } from '@/features/communities/api'
 import { useAuthStore } from '@/stores'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 
-// export function useCreateCommunity() {
-//   return useMutation({ mutationFn: createCommunity })
-// }
-
 export function useCreateCommunity() {
   const queryClient = useQueryClient()
   const userId = useAuthStore((state) => state.user?.id)
