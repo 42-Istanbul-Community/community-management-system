6af69ad67d6642192e7ea161059bd92f69b03058
alikosaca <alikosaca0@gmail.com>
2026-09-14 00:17:13 +0300

    fix(setUser): correct user role assignment from headers

--- services/content/srcs/utils/setUser.js
+++ services/content/srcs/utils/setUser.js
@@ -1,7 +1,7 @@
 function setUser(req, res, next) {
   req.user = {
     id: req.headers['x-user-id'] || null,
-    role: req.headers['x-user-id'] || null,
+    role: req.headers['x-user-role'] || null,
   };
   next();
 }
