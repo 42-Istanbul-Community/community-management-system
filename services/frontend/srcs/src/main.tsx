85bdbac4d88aa6d392262d0c8b9a46b8dbbe1231
yemreaycicek <yunus.e.aycicek@gmail.com>
2026-09-16 16:08:03 +0300

    feat(frontend): add @axe-core/react for accessibility testing

--- services/frontend/srcs/src/main.tsx
+++ services/frontend/srcs/src/main.tsx
@@ -2,9 +2,11 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.tsx'
 import './index.css'
-import { queryClient } from '@/lib'
+import { queryClient, setupAxe } from '@/lib'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+setupAxe()
 
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
