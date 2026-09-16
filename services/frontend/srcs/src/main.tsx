f5dc0dfd850d453ba064fc740fd6e7ae523ee210
yemreaycicek <yunus.e.aycicek@gmail.com>
2026-09-16 16:06:34 +0300

    feat(frontend): add React Query Devtools for improved debugging

--- services/frontend/srcs/src/main.tsx
+++ services/frontend/srcs/src/main.tsx
@@ -4,9 +4,11 @@ import App from './App.tsx'
 import './index.css'
 import { queryClient } from '@/lib'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <App />
+    <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-right" />
   </QueryClientProvider>,
 )
