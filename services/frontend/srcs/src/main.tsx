8fa68225cf58482cd16c029be8ac1a32d73722ab
yemreaycicek <yunus.e.aycicek@gmail.com>
2026-09-16 14:53:33 +0300

    fix(main): remove StrictMode wrapper from the root render

--- services/frontend/srcs/src/main.tsx
+++ services/frontend/srcs/src/main.tsx
@@ -1,4 +1,3 @@
-// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.tsx'
@@ -7,9 +6,7 @@ import { queryClient } from '@/lib'
 import { QueryClientProvider } from '@tanstack/react-query'
 
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
   <QueryClientProvider client={queryClient}>
     <App />
   </QueryClientProvider>,
-  // </StrictMode>,
 )
